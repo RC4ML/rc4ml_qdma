081b6981674823975f63046cf983e3498e3bedb1
yzhblind <yangzihan2000@outlook.com>
2023-09-04 19:04:38 +0800

    replace all printf&cout by fmt

--- QDMAController.cpp
+++ QDMAController.cpp
@@ -1,25 +1,67 @@
 #include "QDMAController.h"
-#include <map>
-using namespace std;
 
-map<char,Bars> device_list;
+#include <cstdint>
+
+#include <map>
+#include <string>
+#include <string_view>
+
+#include <fmt/core.h>
+#include <fmt/chrono.h>
+#include <fmt/ranges.h>
+#include <fmt/os.h>
+#include <fmt/args.h>
+#include <fmt/ostream.h>
+#include <fmt/std.h>	
+#include <fmt/color.h>
+
+struct Bars{
+	volatile uint32_t *config_bar;
+	volatile uint32_t *lite_bar;
+	volatile __m512i *bridge_bar;
+};
+
+typedef struct{
+	int npages;
+	unsigned long* vaddr;
+	unsigned long* paddr;
+}tlb;
+
+std::map<char,Bars> device_list;
 int num_device=0;
 unsigned char default_pci_bus=0;
+
+auto getSysPathBarName(uint8_t bus_id, uint8_t dev_id, uint8_t func_id, uint8_t bar_id) {
+	return fmt::format("/sys/bus/pci/devices/0000:{:02x}:{:02x}.{:x}/resource{}", bus_id, dev_id, func_id, bar_id);
+}
+
+void errorPrint(std::string_view str){
+	fmt::print(fg(fmt::color::red), "{}\n", str);
+}
+
+void passPrint(std::string_view str){
+	fmt::print(fg(fmt::color::green), "{}\n", str);
+}
+
+void warnPrint(std::string_view str){
+	fmt::print(fg(fmt::color::yellow), "{}\n", str);
+}
 
 unsigned char get_pci_bus(unsigned char pci_bus){
 	if(pci_bus==0){
 		pci_bus=default_pci_bus;
 	}
 	if(device_list.count(pci_bus)==0){
-		printf("Device %d has not been initialized\n",pci_bus);
+		errorPrint(fmt::format("Device {:#x} has not been initialized", pci_bus));
 		exit(1);
 	}
 	return pci_bus;
 }
+
 void init(unsigned char pci_bus, size_t bridge_bar_size){
-	printf("Init pci dev: 0x%x\n",pci_bus);
+	passPrint(fmt::format("Init pci dev: 0x{:#x}",pci_bus));
 	if(device_list.count(pci_bus) != 0){
-		printf("device 0x%x has already been initialized!\n",pci_bus);
+		errorPrint(fmt::format("device {:#x} has already been initialized!", pci_bus));
 		exit(1);
 	}else{
 		if(device_list.size() == 0){
@@ -28,45 +70,47 @@ void init(unsigned char pci_bus, size_t bridge_bar_size){
 		Bars t;
 		device_list[pci_bus] = t;
 	}
-	char fname[256];
+	std::string fname;
 	int fd;
 	unsigned char pci_dev 	=	0;
 	unsigned char dev_func	=	0;
 
 	//axi-lite
-	get_syspath_bar_mmap(fname, pci_bus, pci_dev,dev_func, 2);//lite bar is 2
-	fd = open(fname, O_RDWR);
+	fname = getSysPathBarName(pci_bus, pci_dev, dev_func, 2); //lite bar is 2
+
+	fd = open(fname.c_str(), O_RDWR);
 	if (fd < 0){
-		printf("Open lite error, maybe need sudo or you can check whether if %s exists\n",fname);
+		errorPrint(fmt::format("Open lite error, maybe need sudo or you can check whether if {} exists",fname));
 		exit(1);
 	}
 	device_list[pci_bus].lite_bar =(uint32_t*) mmap(NULL, 4*1024, PROT_WRITE, MAP_SHARED, fd, 0);
-	if(device_list[pci_bus].lite_bar == MAP_FAILED){
-		printf("MMAP lite bar error, please check fpga lite bar size in vivado\n");
+	if(device_list[pci_bus].lite_bar == MAP_FAILED) {
+		errorPrint(fmt::format("MMAP lite bar error, please check fpga lite bar size in vivado"));
 		exit(1);
 	}
 	//axi-bridge
-	get_syspath_bar_mmap(fname, pci_bus, pci_dev,dev_func, 4);//bridge bar is 4
-	fd = open(fname, O_RDWR);
-	if (fd < 0){
-		printf("Open bridge error, maybe need sudo or you can check whether if %s exists\n",fname);
+	fname = getSysPathBarName(pci_bus, pci_dev, dev_func, 4); //bridge bar is 4
+
+	fd = open(fname.c_str(), O_RDWR);
+	if (fd < 0) {
+		errorPrint(fmt::format("Open bridge error, maybe need sudo or you can check whether if {} exists",fname));
 		exit(1);
 	}
 	device_list[pci_bus].bridge_bar =(__m512i*) mmap(NULL, bridge_bar_size, PROT_WRITE, MAP_SHARED|MAP_LOCKED , fd, 0);
-	if(device_list[pci_bus].bridge_bar == MAP_FAILED){
-		printf("MMAP bridge bar error, please check fpga bridge bar size in vivado\n");
+	if(device_list[pci_bus].bridge_bar == MAP_FAILED) {
+		errorPrint(fmt::format("MMAP bridge bar error, please check fpga bridge bar size in vivado"));
 		exit(1);
 	}
 	//config bar
-	get_syspath_bar_mmap(fname, pci_bus, pci_dev,dev_func, 0);//config bar is 0
-	fd = open(fname, O_RDWR);
+	fname = getSysPathBarName(pci_bus, pci_dev, dev_func, 0); //config bar is 0
+	fd = open(fname.c_str(), O_RDWR);
 	if (fd < 0){
-		printf("Open config error, maybe need sudo or you can check whether if %s exists\n",fname);
+		errorPrint(fmt::format("Open config error, maybe need sudo or you can check whether if {} exists",fname));
 		exit(1);
 	}
 	device_list[pci_bus].config_bar = (uint32_t *)mmap(NULL, 256*1024, PROT_WRITE, MAP_SHARED, fd, 0);
 	if(device_list[pci_bus].config_bar == MAP_FAILED){
-		printf("MMAP config bar error, please check fpga config bar size in vivado\n");
+		errorPrint(fmt::format("MMAP config bar error, please check fpga config bar size in vivado"));
 		exit(1);
 	}
 }
@@ -76,28 +120,33 @@ void* qdma_alloc(size_t size, unsigned char pci_bus, bool print_addr){
 	int fd,hfd;
 	void* huge_base;
 	struct huge_mem hm;
-	if ((fd = open("/dev/rc4ml_dev",O_RDWR)) == -1) {
-		printf("[ERROR] on open /dev/rc4ml_dev, maybe you need to add 'sudo', or insmod\n");
+	std::string dev_path = fmt::format("/dev/rc4ml_dev");
+	if ((fd = open(dev_path.c_str() ,O_RDWR)) == -1) {
+		errorPrint(fmt::format("Open {rc4ml_dev} error, maybe need sudo or you can check whether if {rc4ml_dev} exists", fmt::arg("rc4ml_dev", dev_path)));
 		exit(1);
    	}
-	char path[20];
-	sprintf(path,"/media/huge/hfd_%x",pci_bus);
-	if ((hfd = open(path, O_CREAT | O_RDWR | O_SYNC, 0755)) == -1) {
-		printf("[ERROR] on open %s, maybe you need to add 'sudo'\n",path);
+
+	std::string hfd_path = fmt::format("/media/huge/hfd_{:x}",pci_bus);
+	if ((hfd = open(hfd_path.c_str(), O_CREAT | O_RDWR | O_SYNC, 0755)) == -1) {
+		errorPrint(fmt::format("Open {fn} error, maybe need sudo or you can check whether if {fn} exists", fmt::arg("fn", hfd_path)));
 		exit(1);
    	}
+
 	huge_base = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, hfd, 0);
-	printf("huge pages base vaddr:%p\n", huge_base);
+	passPrint(fmt::format("huge pages base vaddr:{}", fmt::ptr(huge_base)));
+
 	hm.vaddr = (unsigned long)huge_base;
 	hm.size = size;
 	if(ioctl(fd, HUGE_MAPPING_SET, &hm) == -1){
-		printf("IOCTL SET failed.\n");
+		errorPrint(fmt::format("IOCTL SET failed."));
+		exit(1);
 	}
 	struct huge_mapping map;
 	map.nhpages = size/(2*1024*1024);
 	map.phy_addr = (unsigned long*) calloc(map.nhpages, sizeof(unsigned long*));
 	if (ioctl(fd, HUGE_MAPPING_GET, &map) == -1) {
-    	printf("IOCTL GET failed.\n");
+		errorPrint(fmt::format("IOCTL GET failed."));
+		exit(1);
    	}
 
 	tlb* page_table = (tlb*)calloc(1,sizeof(tlb));
@@ -111,7 +160,7 @@ void* qdma_alloc(size_t size, unsigned char pci_bus, bool print_addr){
 
 	for(int i=0;i<page_table->npages;i++){
 		if(print_addr){
-			printf("%lx %lx\n",page_table->vaddr[i],page_table->paddr[i]);
+			fmt::println("VAddr: {:#016x} PAddr: {:#016x}", page_table->vaddr[i], page_table->paddr[i]);
 		}
 		device_list[pci_bus].lite_bar[8]	= (uint32_t)(page_table->vaddr[i]);
 		device_list[pci_bus].lite_bar[9]	= (uint32_t)((page_table->vaddr[i])>>32);
@@ -172,46 +221,59 @@ void resetCounters(unsigned char pci_bus){
 void printCounters(unsigned char pci_bus){
 	pci_bus = get_pci_bus(pci_bus);
 	volatile uint32_t *axi_lite = device_list[pci_bus].lite_bar;
-	cout<<endl<<"QDMA debug info:"<<endl<<endl;
-	printf("bar 1: 0x%x 	tlb miss count\n",	axi_lite[512+1]);
+	warnPrint("\nQDMA debug info:\n");
 
-	cout<<endl<<"C2H CMD fire()"<<endl;
-	printf("bar 2: 0x%x       io.c2h_cmd\n",	axi_lite[512+2]);
-	printf("bar 3: 0x%x       check_c2h.io.out\n",	axi_lite[512+3]);
-	printf("bar 5: 0x%x       tlb.io.c2h_out\n",	axi_lite[512+4]);
-	printf("bar 4: 0x%x       boundary_split.io.cmd_out\n",	axi_lite[512+5]);
-	printf("bar 6: 0x%x       fifo_c2h_cmd.io.out\n",	axi_lite[512+6]);
+	warnPrint(fmt::format("bar 1: {:#x} 	tlb miss count", axi_lite[512+1]));
 
-	cout<<endl<<"H2C CMD fire()"<<endl;
-	printf("bar 7: 0x%x       io.h2c_cmd\n",	axi_lite[512+7]);
-	printf("bar 8: 0x%x       check_h2c.io.out\n",	axi_lite[512+8]);
-	printf("bar 9: 0x%x       tlb.io.h2c_out\n",	axi_lite[512+9]);
-	printf("bar 10: 0x%x      fifo_h2c_cmd.io.out\n",	axi_lite[512+10]);
+	warnPrint("\nC2H CMD fire()");
 
-	cout<<endl<<"C2H DATA fire()"<<endl;
-	printf("bar 11: 0x%x      io.c2h_data\n",	axi_lite[512+11]);
-	printf("bar 12: 0x%x      boundary_split.io.data_out\n",	axi_lite[512+12]);
-	printf("bar 13: 0x%x      fifo_c2h_data.io.out\n",	axi_lite[512+13]);
+	warnPrint(fmt::format("bar 2: {:#x} 	io.c2h_cmd", axi_lite[512+2]));
+	warnPrint(fmt::format("bar 3: {:#x} 	check_c2h.io.out", axi_lite[512+3]));
+	warnPrint(fmt::format("bar 4: {:#x} 	tlb.io.c2h_out", axi_lite[512+4]));
+	warnPrint(fmt::format("bar 5: {:#x} 	boundary_split.io.cmd_out", axi_lite[512+5]));
+	warnPrint(fmt::format("bar 6: {:#x} 	fifo_c2h_cmd.io.out", axi_lite[512+6]));
 
-	cout<<endl<<"H2C DATA fire()"<<endl;
-	printf("bar 14: 0x%x      io.h2c_data\n",	axi_lite[512+14]);
-	printf("bar 15: 0x%x      fifo_h2c_data.io.in\n",	axi_lite[512+15]);
+	warnPrint("\nH2C CMD fire()");
+	warnPrint(fmt::format("bar 7: {:#x} 	io.h2c_cmd", axi_lite[512+7]));
+	warnPrint(fmt::format("bar 8: {:#x} 	check_h2c.io.out", axi_lite[512+8]));
+	warnPrint(fmt::format("bar 9: {:#x} 	tlb.io.h2c_out", axi_lite[512+9]));
+	warnPrint(fmt::format("bar 10: {:#x} 	boundary_split.io.cmd_out", axi_lite[512+10]));
+
+	warnPrint("\nC2H DATA fire()");
+	warnPrint(fmt::format("bar 11: {:#x} 	io.c2h_data", axi_lite[512+11]));
+	warnPrint(fmt::format("bar 12: {:#x} 	boundary_split.io.data_out", axi_lite[512+12]));
+	warnPrint(fmt::format("bar 13: {:#x} 	fifo_c2h_data.io.out", axi_lite[512+13]));
+
+	warnPrint("\nH2C DATA fire()");
+	warnPrint(fmt::format("bar 14: {:#x} 	io.h2c_data", axi_lite[512+14]));
+	warnPrint(fmt::format("bar 15: {:#x} 	fifo_h2c_data.io.in", axi_lite[512+15]));
 
 	//reporter
-	cout<<endl;
-	cout<<((axi_lite[512+16]>>0) & 1) << " Report 0:boundary check state===sIDLE"<<endl;
-	cout<<((axi_lite[512+16]>>1) & 1) << " Report 1:boundary check state===sIDLE"<<endl;
-	cout<<((axi_lite[512+16]>>2) & 1) << " Report 2:boundary split state===sIDLE"<<endl;
-	cout<<endl;
-	cout<<((axi_lite[512+16]>>3) & 1) << " Report 3:fifo_c2h_cmd.io.out.valid"<<endl;
-	cout<<((axi_lite[512+16]>>4) & 1) << " Report 4:fifo_c2h_cmd.io.out.ready"<<endl;
-	cout<<endl;
-	cout<<((axi_lite[512+16]>>5) & 1) << " Report 5:fifo_h2c_cmd.io.out.valid"<<endl;
-	cout<<((axi_lite[512+16]>>6) & 1) << " Report 6:fifo_h2c_cmd.io.out.ready"<<endl;
-	cout<<endl;
-	cout<<((axi_lite[512+16]>>7) & 1) << " Report 7:fifo_c2h_data.io.out.valid"<<endl;
-	cout<<((axi_lite[512+16]>>8) & 1) << " Report 8:fifo_c2h_data.io.out.ready"<<endl;
-	cout<<endl;
-	cout<<((axi_lite[512+16]>>9) & 1)  << " Report 9:fifo_h2c_data.io.in.valid"<<endl;
-	cout<<((axi_lite[512+16]>>10) & 1)  << " Report 10:fifo_h2c_data.io.in.ready"<<endl;
+	warnPrint("\nReporter");
+	
+	warnPrint(fmt::format("{} Report 0:boundary check state===sIDLE", ((axi_lite[512+16]>>0) & 1)));
+	warnPrint(fmt::format("{} Report 1:boundary check state===sIDLE", ((axi_lite[512+16]>>1) & 1)));
+	warnPrint(fmt::format("{} Report 2:boundary split state===sIDLE", ((axi_lite[512+16]>>2) & 1)));
+
+	fmt::print("\n");
+
+	warnPrint(fmt::format("{} Report 3:fifo_c2h_cmd.io.out.valid", ((axi_lite[512+16]>>3) & 1)));
+	warnPrint(fmt::format("{} Report 4:fifo_c2h_cmd.io.out.ready", ((axi_lite[512+16]>>4) & 1)));
+
+	fmt::print("\n");
+
+	warnPrint(fmt::format("{} Report 5:fifo_h2c_cmd.io.out.valid", ((axi_lite[512+16]>>5) & 1)));
+	warnPrint(fmt::format("{} Report 6:fifo_h2c_cmd.io.out.ready", ((axi_lite[512+16]>>6) & 1)));
+
+	fmt::print("\n");
+
+	warnPrint(fmt::format("{} Report 7:fifo_c2h_data.io.out.valid", ((axi_lite[512+16]>>7) & 1)));
+	warnPrint(fmt::format("{} Report 8:fifo_c2h_data.io.out.ready", ((axi_lite[512+16]>>8) & 1)));
+
+	fmt::print("\n");
+
+	warnPrint(fmt::format("{} Report 9:fifo_h2c_data.io.in.valid", ((axi_lite[512+16]>>9) & 1)));
+	warnPrint(fmt::format("{} Report 10:fifo_h2c_data.io.in.ready", ((axi_lite[512+16]>>10) & 1)));
+
+	fmt::print("\n");
 }
--- QDMAController.h
+++ QDMAController.h
@@ -8,12 +8,6 @@
 #include <immintrin.h>
 #include <rc4ml.h>
 
-struct Bars{
-	volatile uint32_t *config_bar;
-	volatile uint32_t *lite_bar;
-	volatile __m512i *bridge_bar;
-};
-
 void init(unsigned char pci_bus=0x1a,size_t bridge_bar_size=1*1024*1024*1024);
 void writeConfig(uint32_t index,uint32_t value,unsigned char pci_bus=0);
 uint32_t readConfig(uint32_t index,unsigned char pci_bus=0);
@@ -24,16 +18,6 @@ void writeBridge(uint32_t index, uint64_t* value,unsigned char pci_bus=0);
 void readBridge(uint32_t index, uint64_t* value,unsigned char pci_bus=0);
 void* getBridgeAddr(unsigned char pci_bus=0);
 void* getLiteAddr(unsigned char pci_bus=0);
-#define get_syspath_bar_mmap(s, bus,dev,func,bar) \
-	snprintf(s, sizeof(s), \
-		"/sys/bus/pci/devices/0000:%02x:%02x.%x/resource%u", \
-		bus, dev, func, bar)
-
-typedef struct{
-	int npages;
-	unsigned long* vaddr;
-	unsigned long* paddr;
-}tlb;
 
 void resetCounters(unsigned char pci_bus=0);
 void printCounters(unsigned char pci_bus=0);
